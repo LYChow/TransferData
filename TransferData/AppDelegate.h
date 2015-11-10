b0c2f8460e3dfd5be57ad363e285dd605663c939
周路尧 <zhouluyao@meicai.cn>
2015-11-10 21:18:08 +0800

    Initial Commit

---
+++ TransferData/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TransferData
+//
+//  Created by lychow on 11/10/15.
+//  Copyright © 2015 IOSDeveloper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
